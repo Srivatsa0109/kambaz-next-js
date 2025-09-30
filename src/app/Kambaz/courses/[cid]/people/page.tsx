aa681e30acc06c4f8a3650c9007fc58748b29083
Srivatsa0109 <ssrivatsa.jobs@gmail.com>
2025-09-29 23:28:06 -0400

    Delete src/app/Kambaz directory

--- src/app/Kambaz/courses/[cid]/people/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function People() {
-    return (
-        <div>
-            <h1>People</h1>
-            <p>This is a static page which lets us to do changes to people section here.</p>
-        </div>
-    );
-}
